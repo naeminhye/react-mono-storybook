772ac1604fb88efdce00fefebe526dd947ea0106
Hieu Nguyen <hieunm.citynow@gmail.com>
2020-04-20 23:49:13 +0700

    Added Table data

--- _stories/1-basic/Table/data.js
+++ _stories/1-basic/Table/data.js
@@ -36,10 +36,25 @@ const dataSource = [
     email: "janie_marvi6@yahoo.com",
   },
   {
-    name: "Hieu Nguyen",
+    name: "Jennifer G Anderson",
+    gender: "Female",
+    phone: "256-932-7187",
+    add: "4046 New Creek Road, Alexander City, Alabama(AL), 35010",
+    email: "adolfo_cai8@yahoo.com",
+  },
+  {
+    name: "Richard V Garza",
     gender: "Male",
-    phone: "0362467181",
-    add: "297/1 Nguyen Dinh Chieu, Ward 3, Ho Chi Minh City",
+    phone: "562-964-1656",
+    add: "130 Pin Oak Drive, Santa Fe Springs, California(CA), 90670",
+    email: "foster1996@yahoo.com",
+  },
+  {
+    name: "Lane B Wolfenbarger",
+    gender: "Female",
+    phone: "715-849-9361",
+    add: "2224 Tea Berry Lane, Wausau, Wisconsin(WI), 54403",
+    email: "nick_oconne@yahoo.com",
   },
 ];
 
